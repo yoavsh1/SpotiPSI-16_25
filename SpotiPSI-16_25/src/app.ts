aaec1616fe4a2e94d6dd4d45762fee716f2d4165
Yoav Shitrit <yoavshitrit123@gmail.com>
2026-03-17 18:19:20 +0200

    added player and header

---
+++ SpotiPSI-16_25/src/app.ts
@@ -0,0 +1,46 @@
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()(() => ({
+    mainContainer: {
+        backgroundColor: 'black',
+        margin: "0",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", 
+        width: "100%"
+    },
+    header: {
+        backgroundColor: '#333333',
+        height: '60px', 
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        color: "DarkOrchid"
+    },
+    mainSection: {
+        backgroundColor: 'black', 
+        flex: 1,
+        display: 'flex',
+    },
+    sidebar: {
+        backgroundColor: 'black',
+        width: '240px', 
+        borderLeft: '1px solid grey' 
+    },
+    PageContent: {
+        backgroundColor: 'black',
+        flex: 1, 
+    },
+    player: {
+        backgroundColor: '#3333',
+        height: '80px',
+    },
+    textPlay:{
+        color: 'white',
+        textAlign: 'center'
+
+
+    }
+}))
+
+export default useStyles;
