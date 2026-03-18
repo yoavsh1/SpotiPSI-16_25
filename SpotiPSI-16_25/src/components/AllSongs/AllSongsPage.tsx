c3bf9a3696e7ba2358a8efb1cd167c023de8dc80
Yoav Shitrit <yoavshitrit123@gmail.com>
2026-03-18 10:43:56 +0200

    added all songs

---
+++ SpotiPSI-16_25/src/components/AllSongs/AllSongsPage.tsx
@@ -0,0 +1,33 @@
+import type { SongsProps } from "../Types.tsx"
+import useStyles from './StyleAllSongsPage.ts'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import AddIcon from '@mui/icons-material/Add';
+
+const ALLSONGS = "כל השירים"
+
+const AllSongsPage = ({ songs }: SongsProps) => {
+    const { classes } = useStyles()
+
+    return (
+        <div>
+            <h1 className={classes.header}>{ALLSONGS}</h1>
+            {songs.map((song) => (
+                <div className={classes.song}>
+                    <div key={song.id} className={classes.songRow}>
+                        <PlayArrowIcon sx={{ color: "purple" }}></PlayArrowIcon>
+                        <span>{song.name} -</span>
+                        <span>{song.artist}</span>
+                        <div className={classes.rightIcons}>
+                            <AddIcon />
+                            <FavoriteBorderIcon />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+
+    
+}
+export default AllSongsPage
